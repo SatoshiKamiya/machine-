fef23a4d61b22b3df22626091f8e039084afd7cf
SatoshiKami <kamiya.satoshi@opensesame-tech.com>
2024-09-24 23:00:35 +0900

    ADD　mainファイル追加

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def main():
+    print("ここから始まるよ!!")
+
+
+if __name__ == "__main__":
+    main()
