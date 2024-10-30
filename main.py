3d521d1b8bebe4f756c98e17ddfe547347b31a39
SatoshiKami <kamiya.satoshi@opensesame-tech.com>
2024-10-30 23:11:56 +0900

    FINISH　スコア算出　グリッチサーチによりハイパーパラメータ最適化

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import cross_validate
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
 
 
 def main():
@@ -64,35 +66,65 @@ def main():
     test = df[df["Survived"].isnull()].drop("Survived", axis=1)
 
     # データフレームをnumpyに変換
-    X = train.values[:, 1:] #トレーニングデータの1行目以降取得
-    y = train.values[:, 0] #トレーニングデータの0行目のみ取得
-    test_x = test.values
+    X = train.values[:, 1:]  # トレーニングデータの1行目以降取得
+    y = train.values[:, 0]  # トレーニングデータの0行目のみ取得
+    # test_x = test.values
 
+    # print("----------------- gred serch start --------------------")
+    # # グリッドサーチのためのデータ分割
+    # # test_size:テストデータの割合　0.2=20%
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     X, y, test_size=0.2, random_state=42
+    # )
 
-    # モデルへ値を渡す
-    # 採用する特徴量を25個から4個に絞り込む　←　自動で特徴量を選定してくれる
-    # 今回は絞らない（特徴量が少ないため）
-    # select = SelectKBest(k=4)
+    # # モデルのインスタンスを作成
+    # model = RandomForestClassifier(random_state=10, warm_start=True)
+    # # 試したいパラメータのリスト
+    # param_grid = {
+    #     "n_estimators": [15, 25, 50, 100, 150, 300, 450],
+    #     "max_depth": [3, 5, 7, 8, 9, 10, 15, 20, 25],
+    # }
 
-    # ランダムフォレスト（2値分類）
+    # # グリッドサーチ
+    # grid_search = GridSearchCV(model, param_grid, cv=5, scoring="accuracy")
+    # grid_search.fit(X_train, y_train)
+
+    # # 最適なパラメータとスコアの表示
+    # print("Best parameters:", grid_search.best_params_)
+    # print("Best cross-validation score:", grid_search.best_score_)
+
+    # # テストデータに対する精度
+    # best_model = grid_search.best_estimator_
+    # y_pred = best_model.predict(X_test)
+    # print("Test accuracy:", accuracy_score(y_test, y_pred))
+
+    # # Best parameters: {'max_depth': 8, 'n_estimators': 300}
+    # # Best cross-validation score: 0.8370432384516893
+    # # Test accuracy: 0.8212290502793296
+    # print("----------------- gred serch end --------------------")
+
+    # ----------------------------------------------------------------------------------------
+    print("----------------- trainig start --------------------")
+    # ランダムフォレスト（2値分類）　ハイパーパラメータが決まったらこれを利用する
     clf = RandomForestClassifier(
         random_state=10,  # ランダムで得られる数値（数値を指定すれば固定される）
         warm_start=True,  # 既にフィットしたモデルに学習を追加
-        n_estimators=100,  # 使用する決定木の数
-        max_depth=6,      # 各決定木の深さの最大値
-        max_features="sqrt", # 各決定木で使用する特徴量の数
+        n_estimators=300,  # 使用する決定木の数
+        max_depth=8,  # 各決定木の深さの最大値
+        max_features="sqrt",  # 各決定木で使用する特徴量の数
     )
 
-    #ハイパーパラメータ調整するなら「 グリッドサーチ」をやろう
-
-    # pipeline = make_pipeline(select, clf)
     pipeline = make_pipeline(clf)
     pipeline.fit(X, y)
 
     # フィット結果の表示
     cv_result = cross_validate(pipeline, X, y, cv=10)
-    print("mean_score = ", np.mean(cv_result["test_score"])) # 結果：mean_score = 0.8317353308364543
-    print("mean_std = ", np.std(cv_result["test_score"]))    # 結果：mean_std = 0.04801731838866762
+    print(
+        "mean_score = ", np.mean(cv_result["test_score"])
+    )  # 結果：mean_score = 0.8440324594257177
+    print(
+        "mean_std = ", np.std(cv_result["test_score"])
+    )  # 結果：mean_std = 0.04348198471288303
 
 
 if __name__ == "__main__":
