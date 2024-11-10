3da239de93d9e82eb82b1c89c9e37795548c59cb
SatoshiKami <kamiya.satoshi@opensesame-tech.com>
2024-11-10 20:53:35 +0900

    END　house prices提出

--- main_house_prices.py
+++ main_house_prices.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.experimental import enable_iterative_imputer
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import IterativeImputer
 from sklearn.impute import SimpleImputer
@@ -47,12 +45,6 @@ def main():
             "OpenPorchSF",
         ]
     ]
-    # 相関係数チェック
-    # num_total_df_corr = num_total_df.corr()
-    # print("num_total_df　補完前相関チェック")
-    # print(num_total_df_corr)
-
-    # 文字列は別途用意　まずは数値のみデータの外れ値と欠損値補完する
 
     # -----------------------------------------------------------------------------------------
 
@@ -178,19 +170,11 @@ def main():
     # 説明変数・目的変数
     X = num_total_df_traning.drop("SalePrice", axis=1)  # 説明変数
     y = num_total_df_traning["SalePrice"]  # 目的変数
-    # データの前処理（例：欠損値補完やエンコーディング）
-    # X = pd.get_dummies(X, drop_first=True)  # ワンホットエンコーディングなどの前処理を適用
 
     # 訓練データとテストデータに分割
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
-    # print("型＆nullチェック　X_train確認")
-    # print(X_train.info())
-    # print(X_train.describe())
-    # print("型＆nullチェック y_train確認")
-    # print(y_train.info())
-    # print(y_train.describe())
 
     # 学習
     # XGBoostモデルの定義と学習
@@ -244,131 +228,6 @@ def main():
     print(submission.describe())
     print(submission.head(1500))
     submission.to_csv("submission.csv", index=False)
-
-
-# -----------------------------------------------------------------------------------------
-# # 相関係数
-# numeric_df = train_df.select_dtypes(include=[float, int])
-# # カラムチェック
-# print("カラムの種類", numeric_df.columns)
-
-# salePrices check
-# print("セール価格の統計値")
-# print(train_df["SalePrice"].describe())
-
-# ---------------------------One-Hot Encoding相関関係チェック--------------------------------
-# print("Neighborhood")
-# print(train_df["Neighborhood"].value_counts())
-# print("ExterQual")
-# print(train_df["ExterQual"].value_counts())
-# print("BsmtQual")
-# print(train_df["BsmtQual"].value_counts())
-# print("KitchenQual")
-# print(train_df["KitchenQual"].value_counts())
-# print("GarageFinish")
-# print(train_df["GarageFinish"].value_counts())
-
-# train_df = train_df[["SalePrice", "ExterQual", "BsmtQual", "BsmtQual"]]
-
-# One-Hot Encodingを適用
-# df_encoded = pd.get_dummies(train_df, columns=["ExterQual"]).astype(int)
-# df_encoded = pd.get_dummies(train_df).astype(int)
-
-# print("中間チェック")
-# print(df_encoded)
-# 相関を計算
-# bsmtqual_columns = [
-#     col for col in df_encoded.columns if col.startswith("ExterQual" + "_")
-# ]
-# correlation_matrix = df_encoded.corr()["SalePrice"][bsmtqual_columns]
-
-# print("SalePrice vs GarageFinish")
-# print(correlation_matrix)
-
-# ---------------------------文字列データ選定--------------------------------
-# cor_str_train_df = train_df[
-#     [
-#         "Neighborhood",
-#         "ExterQual",
-#         "BsmtQual",
-#         "KitchenQual",
-#         "GarageFinish",
-#     ]
-# ]
-# print("info=", cor_str_train_df.info())
-# `---------------------------文字列内の相関選定--------------------------------
-# cor_str_train_df_01 = train_df[
-#     [
-#         "SalePrice",
-#         "Street",
-#         "Alley",
-#         "LotShape",
-#         "LandContour",
-#         "Utilities",
-#         "LotConfig",
-#         "LandSlope",
-#         "Neighborhood",
-#         "Condition1",
-#         "Condition2",
-#         "BldgType",
-#         "HouseStyle",
-#     ]
-# ]
-
-# pd.set_option("display.max_rows", 500)
-# print("head")
-# print(cor_str_train_df_01.head(300))
-
-# `---------------------------相関係数による相関選定----------------------------
-# cor_train_df_01 = train_df[
-#     [
-#         "SalePrice",
-#         "LotFrontage",
-#         "OverallQual",
-#         "YearBuilt",
-#         "YearRemodAdd",
-#         "MasVnrArea",
-#         "BsmtFinSF1",
-#         "TotalBsmtSF",
-#         "1stFlrSF",
-#         "2ndFlrSF",
-#         "GrLivArea",
-#         "FullBath",
-#     ]
-# ]
-
-# cor_train_df_02 = train_df[
-#     [
-#         "SalePrice",
-#         "TotRmsAbvGrd",
-#         "Fireplaces",
-#         "GarageYrBlt",
-#         "GarageCars",
-#         "GarageArea",
-#         "WoodDeckSF",
-#         "OpenPorchSF",
-#     ]
-# ]
-
-# cor_train_df_03 = train_df[
-#     [
-#         "SalePrice",
-#         "BedroomAbvGr",
-#         "KitchenAbvGr",
-#     ]
-# ]
-
-# cor = cor_train_df_03.corr()
-# sns.heatmap(
-#     cor,
-#     cmap=sns.color_palette("coolwarm", 10),
-#     annot=True,
-#     fmt=".2f",
-#     vmin=-1,
-#     vmax=1,
-# )
-# plt.show()
-# print(correlation_matrix)
 
 
 if __name__ == "__main__":
