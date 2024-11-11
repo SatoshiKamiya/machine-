b3d0ba087eda7b37246473c899976125c41f1bea
kamiya <kamiya.satoshi@opensesame-tech.com>
2024-11-11 12:55:15 +0900

    ADD　欠損値チェック　NEXT　相関係数チェック

--- trial_and_error_chp_main.py
+++ trial_and_error_chp_main.py
@@ -6,7 +6,6 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import IterativeImputer
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-import xgboost as xgb
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
@@ -14,14 +13,25 @@ from sklearn.preprocessing import StandardScaler
 def main():
     print("main処理開始")
     # 1つ目のモデル用
-    train_df = pd.read_csv("data/house_prices/train.csv")
-    test_df = pd.read_csv("data/house_prices/test.csv")
-    total_df = pd.concat([train_df, test_df], ignore_index=True, sort=False)
-    # print("tranigデータ数：", train_df.shape[0]) # 1460
-    # print("testデータ数：", test_df.shape[0]) # 1459
+    train_df = pd.read_csv("data/customer_transaction_prediction/train.csv") # (200000, 202)
+    train_df_first = train_df.iloc[:,:100] # メモリ不足のため分割（前半）
+    train_df_second = train_df.iloc[:,100:150] # メモリ不足のため分割（中）
+    train_df_third = train_df.iloc[:,150:] # メモリ不足のため分割（後半）
+    test_df = pd.read_csv("data/customer_transaction_prediction/test.csv") # (200000, 201)
+    test_df_first = test_df.iloc[:,:100] # メモリ不足のため分割（前半）
+    test_df_second = test_df.iloc[:,100:150] # メモリ不足のため分割（中）
+    test_df_third = test_df.iloc[:,150:] # メモリ不足のため分割（後半）
 
-    # 列数50まで表示
-    pd.set_option("display.max_columns", 50)
+    total_df = pd.concat([train_df, test_df], ignore_index=True, sort=False)
+    print("tranigデータ数：", train_df.shape) # 1460
+    print("testデータ数：", test_df.shape) # 1459
+
+    # # 列数50まで表示
+    # pd.set_option("display.max_columns", 50)
+
+    # 欠損値チェック # なし
+    # print(train_df_first.info()) #なし
+    
 
     # ---------------------------カラム相関チェック後抽出後データ--------------------------------
 
