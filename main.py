60d0d838a68b9e9d8b6bb33360a924937f0f40a0
kamiya <kamiya.satoshi@opensesame-tech.com>
2024-11-18 12:55:08 +0900

    ADD　調査内容追加

--- main.py
+++ main.py
@@ -8,22 +8,21 @@ def main():
     pd.set_option("display.max_columns", None)  # 列の表示数の制限を解除
     pd.set_option("display.max_rows", None)  # 行の表示数の制限を解除
     # 1つ目のモデル用
-    train_df = pd.read_csv(
-        "data/train.csv"
-    )  # (200000, 202)
+    train_df = pd.read_csv("data/train.csv") 
 
-        # 1つ目のモデル用
-    test_df = pd.read_csv(
-        "data/test.csv"
-    )  # (200000, 202)
+    # 1つ目のモデル用
+    test_df = pd.read_csv("data/test.csv") 
+
+     # トレーニングデータとテストデータを合体させる
+    # test_df = pd.read_csv("data/test.csv") 
 
     print("train_df　行列数")
-    print(train_df.shape) 
+    print(train_df.shape)  # (58645, 13)
     print("test_df　行列数")
-    print(test_df.shape) 
+    print(test_df.shape)  # (39098, 12)
 
     print("train_df　head")
-    print(train_df.head()) 
+    print(train_df.head(50)) 
 
     print("train_df　統計量")
     print(train_df.describe())
@@ -33,9 +32,20 @@ def main():
     print("test_df　欠損値")
     print(test_df.info())
 
+    print("相関係数チェック")
+    corr_matrix = train_df.select_dtypes(include="number").corr() #　数値のみのカラム抽出
+    print("相関係数チェック　ターゲット")
+    print(corr_matrix["loan_status"])
+    print("相関係数チェック　全体")
+    print(corr_matrix)
+
+    print("値の分布チェック")
+
     print("train_df　ターゲット値可視化")
     train_df.hist(figsize=(10, 10), bins=30)
     plt.show()
+
+    
 
 if __name__ == "__main__":
     main()
