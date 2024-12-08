6b6ccbd7f44ddefda1775f7d1a9f607d2f29fba1
SatoshiKami <kamiya.satoshi@opensesame-tech.com>
2024-12-08 23:03:21 +0900

    NEW　新規実装開始

--- documents/kaggler手法調査.md
+++ documents/kaggler手法調査.md
@@ -4,22 +4,72 @@
 # Kaggler手法研究
 
 ##  Kaggler
-
+Abdallah Wagih Ibrahim
 
 ## 関連サイト
-
+NLP - Transformers
+https://www.kaggle.com/code/abdallahwagih/nlp-transformers
 
 ##  予測精度
 
 
 ##  モデル
-- 
+- transformers
 
 ##  処理の流れ
+■MultiHeadAttentionクラスの定義
+■Encoder blockクラスの定義
+■Encoderクラスの定義
+■Decoder blockクラスの定義
+■Decoderクラスの定義
+■Transformerクラスの定義
 
+■Fine-tuning
+- データの読み込み　
+- データチェック　Id、Review、Ratingを確認
+- ターゲットの分布を調べる　棒グラフ（x軸：rate　y軸：数）で1~5の評価でどのくらい数があるか表示
+■前処理
+- Hugging Face データセット オブジェクトと互換性を持たせるための処理
+- Ratingの値を変更　全体に-1する
+- train_test_splitメソッドを利用してX_train, X_valid, _, _を取得
+- 一旦セーブ　new_train.csv、new_valid.csv
+■トレーニング前準備
+- load_datasetでnew_train.csv、new_valid.csvを呼ぶ
+- トークナイザーの指定　モデルを指定すれば、AutoTokenizerが自動でトークナイザーを指定してくれる
+- モデル　事前トレーニング済みの distilbert-base-uncased モデルを使用
+- GPUにコネクト
+- モデルをダウンロード
+- 評価基準（精度チェック）MAEを利用
+■トレーニング
+- ハイパーパラメータの調整
+- transformersのTrainerを使ってトレーニングする
+- 検証データセットを利用して予測
+- 
+■評価
+- 混同行列をプロット
 - 
 - 
 
+- 
+- 
+- 
+- 
+- 
+- 
+- 
+- 
+
+- 
+- 
+- 
+- 
+- 
+- 
+- 
+
+- 
+- 
+- 
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
+import seaborn as sns
+sns.set(style='darkgrid', font_scale=1.6)
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, accuracy_score, f1_score
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+import os
+os.environ["WANDB_DISABLED"] = "true"
 
-
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import Trainer, TrainingArguments
+import datasets
+from datasets import load_dataset
 def main():
     print("main処理開始")
+    train = pd.read_csv("data/train.csv")
+    test = pd.read_csv("data/test.csv")
+    sub = pd.read_csv("data/sample_submission.csv")
+
+    print("トレーニング　行列数")
+    print(train.shape)
+    print("test　行列数")
+    print(test.shape)
+    print("sub　行列数")
+    print(sub.shape)
+    print("トレーニング　head")
+    print(train.head(5))
+
+    # 棒グラフ
+    plt.figure(figsize=(10,4))
+    sns.countplot(data=train, x="Rating")
+    plt.title("Target distribution")
+    plt.show()
+    
+
+
+
    
 
 if __name__ == "__main__":
