0e2cf1a129d8c91189132f589230839e4961ed27
SatoshiKami <kamiya.satoshi@opensesame-tech.com>
2024-12-09 23:24:20 +0900

    ADD　データセット処理完了まで

--- main.py
+++ main.py
@@ -43,9 +43,25 @@ def main():
     X_train, X_valid, _, _ = train_test_split(train, train["Rating"], test_size=0.2, shuffle=True, random_state=0)
     
     # カラム名を変更　Review → text、Rating → label
-    # 変更したデータを「new_train.csv」に保存する
+    # 変更したデータをX_train「new_train.csv」、X_valid「new_valid.csv」、test「new_test.csv」に保存する
     X_train[["Review","Rating"]].rename(columns={"Review":"text", "Rating":"label"}).to_csv("new_train.csv", index=False)
-    
+    X_valid[["Review","Rating"]].rename(columns={"Review":"text", "Rating":"label"}).to_csv("new_valid.csv", index=False)
+    test[["Review"]].rename(columns={"Review":"text"}).to_csv("new_test.csv", index=False)
+
+    # データセット生成 先ほど作ったcsvファイルのデータを呼び出す
+    train_ds = load_dataset("csv", data_files={"train": "new_train.csv"})
+    valid_ds = load_dataset("csv", data_files={"valid": "new_valid.csv"})
+    test_ds = load_dataset("csv", data_files={"test": "new_test.csv"})
+
+    # データセット内のデータを処理しやすいように再定義
+    # textをString型、labelをラベル付け（ラベルエンコーディング）する
+    train_ds = train_ds.cast(datasets.Features({"text": datasets.Value("string"), "label": datasets.ClassLabel(num_classes=5)}))
+    valid_ds = valid_ds.cast(datasets.Features({"text": datasets.Value("string"), "label": datasets.ClassLabel(num_classes=5)}))
+
+    # Print summary
+    print(train_ds)
+    print(valid_ds)
+    print(test_ds)
 
 
 
